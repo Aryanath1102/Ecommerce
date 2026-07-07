c68d94847303c0999ad7ac93b9fb9c1ba50aea22
Arya Nath <nath.arya2000@gmail.com>
2026-07-07 17:19:13 +0530

    Add new components and enhance existing ones for improved homepage layout and functionality

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -2,26 +2,26 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProduct] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    setLatestProduct(products.slice(0, 10));
+    const bestProduct = products.filter((i) => i.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
   }, []);
 
   return (
-    <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTION"} />
+    <div className="'my-10">
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          neque dolore ex in itaque voluptatibus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+          fugit.
         </p>
       </div>
-      {/*  Rendering Products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
             id={item.id}
@@ -35,4 +35,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
           </h1>
           <div className="flex items-center gap-2">
             <p className="font-semibold text-sm md:text-bold">SHOP NOW</p>
-            <p className="w-8 md:w-11 h-[1px] bg-[#414141]"></p>
+            <p className="w-8 md:w-11 h-px bg-[#414141]"></p>
           </div>
         </div>
       </div>
